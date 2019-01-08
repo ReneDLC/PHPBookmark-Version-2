b4dcfeb371384733091cbcbae626b1616f120011
Rene De La Calleja <renedlc1@gmail.com>
2019-01-07 18:12:26 -0600

    Add project files.

---
+++ PHPBookmark Version 2/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PHPBookmark_Version_2.Startup))]
+namespace PHPBookmark_Version_2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
